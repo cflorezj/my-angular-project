03f00473cd3cc015b1bc5230e4e9be5841b8102a
Martin Vazquez <martin.vazquez@rockwellautomation.com>
2020-12-07 11:36:45 -0300

    [EXOCXEP-238] Copy neccesary classes from AEM CIF to our codebase

---
+++ core/src/main/java/com/rockwell/aem/commerce/core/models/internal/SearchAggregationOptionImpl.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.rockwell.aem.commerce.core.models.internal;
+
+import java.util.Map;
+
+import com.adobe.cq.commerce.core.search.models.SearchAggregationOption;
+
+/**
+ * Basic implementation of {@link SearchAggregationOption}.
+ */
+public class SearchAggregationOptionImpl implements SearchAggregationOption {
+
+    private String filterValue;
+    private String displayLabel;
+    private int count;
+    private Map<String, String> addFilterMap;
+
+    @Override
+    public String getFilterValue() {
+        return filterValue;
+    }
+
+    @Override
+    public String getDisplayLabel() {
+        return displayLabel;
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public Map<String, String> getAddFilterMap() {
+        return addFilterMap;
+    }
+
+    public void setFilterValue(final String filterValue) {
+        this.filterValue = filterValue;
+    }
+
+    public void setDisplayLabel(final String displayLabel) {
+        this.displayLabel = displayLabel;
+    }
+
+    public void setCount(final int count) {
+        this.count = count;
+    }
+
+    public void setAddFilterMap(final Map<String, String> addFilterMap) {
+        this.addFilterMap = addFilterMap;
+    }
+}
